df5794e0f398127583eb24cd55ab1d4e352fe1a8
Marc Schaefer <hi@marcschaefer.de>
2021-05-01 18:32:42 +0200

    Add color selection functionality

--- script.js
+++ script.js
@@ -1,9 +1,81 @@
 'use strict'
 
-let numOfBoxes = 25;
-document.getElementById('numberFields').value = numOfBoxes;
+// We'll have a global variable to store the currently selected color
+let selectedColor = 'hsl(61, 35%, 75%)';
 
-drawPlayground(numOfBoxes);
+// As soon as the DOM has loaded, call the init() function
+document.addEventListener('DOMContentLoaded', init);
+
+/**
+ * Registers all event listeners.
+ * 
+ * @return {void} 
+ */
+
+function init() {
+    let numOfBoxes = 25;
+
+    document.getElementById('numberFields').value = numOfBoxes;
+    document.getElementById('numberFields').addEventListener('keyup', (e) => {
+        if (e.key === 'Enter') {
+            document.querySelector('button').click();
+        }
+    })
+
+    let colors = document.querySelectorAll('.color');
+    for (let i = 0; i < colors.length; i++) {
+        colors[i].addEventListener('click', (e) => {
+            switch (e.target.id) {
+                case 'palespringbud': 
+                    selectedColor = 'hsl(61, 35%, 75%)';
+                    break;
+                case 'cambridgeblue':
+                    selectedColor = 'hsl(154, 18%, 67%)';
+                    break;
+                case 'airsuperiorityblue':
+                    selectedColor = 'hsl(202, 30%, 59%)';
+                    break;
+                case 'bluemunsell':
+                    selectedColor = 'hsl(190, 75%, 37%)';
+                    break;
+                case 'cybergrape':
+                    selectedColor = 'hsl(256, 30%, 38%)';
+                    break;
+                case 'white':
+                    selectedColor = 'hsl(0, 0%, 100%)';
+                    break;
+            }
+        })
+    }
+
+    document.querySelector('button').addEventListener('click', (e) => {
+        let numOfBoxes = document.getElementById('numberFields').value;
+        document.querySelector('.controls-errorMessage').classList.add('invisible');
+
+        // Do we have a number?
+        if (numOfBoxes != null && isNaN(numOfBoxes) === false) {
+            if (numOfBoxes < 1) {
+                document.getElementById('message').textContent = "Should be 1 or greater";
+                document.querySelector('.controls-errorMessage').classList.remove('invisible');
+                return;
+            } else if (numOfBoxes > 99) {
+                document.getElementById('message').textContent = "Should be 99 or smaller";
+                document.querySelector('.controls-errorMessage').classList.remove('invisible');
+                return;
+            }
+        }
+        drawPlayground(numOfBoxes);
+    });
+
+    drawPlayground(numOfBoxes);
+}
+
+/**
+ * Draws a grid with the given number of boxes and attaches it to the DOM.
+ *
+ * @param {number} numOfBoxes The number of boxes to draw in each line of the grid.
+ * @return {void} 
+ */
 
 function drawPlayground(numOfBoxes) {
     clearPlayground();
@@ -23,7 +95,7 @@ function drawPlayground(numOfBoxes) {
             elem.style.width = `${(1/numOfBoxes) * 100}%`;
 
             elem.addEventListener('mouseover', (e) => {
-                e.target.style.backgroundColor = 'black';
+                e.target.style.backgroundColor = selectedColor;
             });
 
             playground.appendChild(elem);
@@ -31,39 +103,13 @@ function drawPlayground(numOfBoxes) {
     }
 }
 
+/**
+ * Deletes the current grid.
+ *
+ * @return {void} 
+ */
+
 function clearPlayground() {
     let playground = document.getElementById('playground');
     playground.remove();
 }
-
-// get handler for all boxes
-let allBoxes = document.querySelectorAll('.box');
-
-for(let i = 0; i < allBoxes.length; i++) {
-    allBoxes[i].addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'black';
-    });
-}
-
-document.getElementById('numberFields').addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-        document.querySelector('button').click();
-    }
-})
-
-document.querySelector('button').addEventListener('click', (e) => {
-    let numOfBoxes = document.getElementById('numberFields').value;
-    document.getElementById('message').textContent = "";
-
-    // Do we have a number?
-    if (numOfBoxes != null && isNaN(numOfBoxes) === false) {
-        if (numOfBoxes < 1) {
-            document.getElementById('message').textContent = "Should be 1 or greater";
-            return;
-        } else if (numOfBoxes > 99) {
-            document.getElementById('message').textContent = "Should be 99 or smaller";
-            return;
-        }
-    }
-    drawPlayground(numOfBoxes);
-});
