a51fcdf1964f8ef1b169317087db23f3e4d79040
Marc Schaefer <hi@marcschaefer.de>
2021-05-01 18:48:19 +0200

    Restructure the JS code

--- script.js
+++ script.js
@@ -6,77 +6,32 @@ let selectedColor = 'hsl(61, 35%, 75%)';
 // As soon as the DOM has loaded, call the init() function
 document.addEventListener('DOMContentLoaded', init);
 
-/**
- * Registers all event listeners.
- * 
- * @return {void} 
- */
-
+// Registers all event listeners.
 function init() {
+    // Let's start with a default value of 25 boxes per row
     let numOfBoxes = 25;
-
     document.getElementById('numberFields').value = numOfBoxes;
+
+    // Adding the event listener for the 'Reset' button
+    document.querySelector('button').addEventListener('click', buttonClick);
+
+    // If the enter key is pressed in the number field, the 'Reset' button should trigger
     document.getElementById('numberFields').addEventListener('keyup', (e) => {
         if (e.key === 'Enter') {
             document.querySelector('button').click();
         }
     })
 
+    // Clicking the color buttons should change the currently selected color
     let colors = document.querySelectorAll('.color');
     for (let i = 0; i < colors.length; i++) {
-        colors[i].addEventListener('click', (e) => {
-            switch (e.target.id) {
-                case 'palespringbud': 
-                    selectedColor = 'hsl(61, 35%, 75%)';
-                    break;
-                case 'cambridgeblue':
-                    selectedColor = 'hsl(154, 18%, 67%)';
-                    break;
-                case 'airsuperiorityblue':
-                    selectedColor = 'hsl(202, 30%, 59%)';
-                    break;
-                case 'bluemunsell':
-                    selectedColor = 'hsl(190, 75%, 37%)';
-                    break;
-                case 'cybergrape':
-                    selectedColor = 'hsl(256, 30%, 38%)';
-                    break;
-                case 'white':
-                    selectedColor = 'hsl(0, 0%, 100%)';
-                    break;
-            }
-        })
+        colors[i].addEventListener('click', colorClick);
     }
-
-    document.querySelector('button').addEventListener('click', (e) => {
-        let numOfBoxes = document.getElementById('numberFields').value;
-        document.querySelector('.controls-errorMessage').classList.add('invisible');
-
-        // Do we have a number?
-        if (numOfBoxes != null && isNaN(numOfBoxes) === false) {
-            if (numOfBoxes < 1) {
-                document.getElementById('message').textContent = "Should be 1 or greater";
-                document.querySelector('.controls-errorMessage').classList.remove('invisible');
-                return;
-            } else if (numOfBoxes > 99) {
-                document.getElementById('message').textContent = "Should be 99 or smaller";
-                document.querySelector('.controls-errorMessage').classList.remove('invisible');
-                return;
-            }
-        }
-        drawPlayground(numOfBoxes);
-    });
 
     drawPlayground(numOfBoxes);
 }
 
-/**
- * Draws a grid with the given number of boxes and attaches it to the DOM.
- *
- * @param {number} numOfBoxes The number of boxes to draw in each line of the grid.
- * @return {void} 
- */
-
+//  Draws a grid with the given number of boxes and attaches it to the DOM.
 function drawPlayground(numOfBoxes) {
     clearPlayground();
 
@@ -103,13 +58,52 @@ function drawPlayground(numOfBoxes) {
     }
 }
 
-/**
- * Deletes the current grid.
- *
- * @return {void} 
- */
-
+//  Deletes the current grid.
 function clearPlayground() {
     let playground = document.getElementById('playground');
     playground.remove();
+}
+
+// Click handler for the Reset button.
+function buttonClick(e) {
+    let numOfBoxes = document.getElementById('numberFields').value;
+    document.querySelector('.controls-errorMessage').classList.add('invisible');
+
+    // Do we have a number?
+    if (numOfBoxes != null && isNaN(numOfBoxes) === false) {
+        if (numOfBoxes < 1) {
+            document.getElementById('message').textContent = "Should be 1 or greater";
+            document.querySelector('.controls-errorMessage').classList.remove('invisible');
+            return;
+        } else if (numOfBoxes > 99) {
+            document.getElementById('message').textContent = "Should be 99 or smaller";
+            document.querySelector('.controls-errorMessage').classList.remove('invisible');
+            return;
+        }
+    }
+    drawPlayground(numOfBoxes);
+}
+
+// Click handler for the color buttons.
+function colorClick(e) {
+    switch (e.target.id) {
+        case 'palespringbud': 
+            selectedColor = 'hsl(61, 35%, 75%)';
+            break;
+        case 'cambridgeblue':
+            selectedColor = 'hsl(154, 18%, 67%)';
+            break;
+        case 'airsuperiorityblue':
+            selectedColor = 'hsl(202, 30%, 59%)';
+            break;
+        case 'bluemunsell':
+            selectedColor = 'hsl(190, 75%, 37%)';
+            break;
+        case 'cybergrape':
+            selectedColor = 'hsl(256, 30%, 38%)';
+            break;
+        case 'white':
+            selectedColor = 'hsl(0, 0%, 100%)';
+            break;
+    }
 }
